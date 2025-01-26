714ae70f3cd1f19042f96c0a461bcc56e1f0f994
pranay-janjarla <pranaymessi12345@gmail.com>
2025-01-27 04:55:44 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.querySelector(".carousel");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  carousel.addEventListener("mousedown", (e) => {
+    isDown = true;
+    carousel.classList.add("active");
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+  });
+
+  carousel.addEventListener("mouseleave", () => {
+    isDown = false;
+    carousel.classList.remove("active");
+  });
+
+  carousel.addEventListener("mouseup", () => {
+    isDown = false;
+    carousel.classList.remove("active");
+  });
+
+  carousel.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 2;
+    carousel.scrollLeft = scrollLeft - walk;
+  });
+});
